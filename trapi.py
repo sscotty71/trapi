7243e866cde0a3276365bb4374b21169e96f0dd8
Enrico <sscotty71@gmail.com>
2024-09-18 16:08:00 +0200

    text area added

--- trapi.py
+++ trapi.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, request, render_template
 from transformers import MarianMTModel, MarianTokenizer
 
-# Carica il modello e il tokenizer
-model_name = 'Helsinki-NLP/opus-mt-tc-big-en-fr'
-tokenizer = MarianTokenizer.from_pretrained(model_name)
-model = MarianMTModel.from_pretrained(model_name)
+# Carica il modello1 e il tokenizer
+model_name1 = 'Helsinki-NLP/opus-mt-tc-big-en-it'
+tokenizer = MarianTokenizer.from_pretrained(model_name1)
+model1 = MarianMTModel.from_pretrained(model_name1)
 
 app = Flask(__name__)
 
@@ -17,9 +17,10 @@ def submit():
     source = request.form['source']
     src_text = [source]
     print (f'{source}')
-    translated = model.generate(**tokenizer(src_text, return_tensors="pt", padding=True))
-    out_text = tokenizer.decode(translated[0], skip_special_tokens=True)
-    return f"Source: {source} / Translation: {out_text}"
+    translated = model1.generate(**tokenizer(src_text, return_tensors="pt", padding=True))
+    out_text1 = tokenizer.decode(translated[0], skip_special_tokens=True)
+    return f"<b>Source:</b> {source} <BR/<BR/><b>Translation:</b> {out_text1} <BR/>"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0') 
